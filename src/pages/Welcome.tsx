bef211015f594040f202fabc827f1e00047d7c11
Nia Kostadinova <n_deyanova@abv.bg>
2024-11-26 20:24:48 +0200

    Add text on Welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -4,8 +4,10 @@ const Welcome: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
         style={{ backgroundImage: `url(${welcomeImg})` }}>
-            <h1 className="text-4xl font-bold text-white shadow-md">Welcome</h1>
-            <p className="text-lg text-white">This is your welcome page!</p>
+            <div className="bg-gray-700 bg-opacity-50 p-6 rounded-lg items-center flex flex-col">
+                <h1 className="text-4xl font-bold text-white pb-4">VRScans Materials Library</h1>
+                <p className="text-lg text-white">An intuitive, React-powered application that dynamically fetches and displays VRScans materials via a pre-configured REST API.</p>
+            </div>
         </div>
       );
 };
