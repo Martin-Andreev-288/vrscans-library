6680ff0e52aa3c775e874d82a5add38464f2d22a
Nia Kostadinova <n_deyanova@abv.bg>
2024-11-26 20:52:11 +0200

    Fix Welcome background img to have 100vh height

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,9 +1,9 @@
-import welcomeImg from "../assets/welcome-page-img.png";
+import welcomeImg from "../assets/welcome-page-img-2.png";
 
 const Welcome: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
-        style={{ backgroundImage: `url(${welcomeImg})` }}>
+        <div   className="flex flex-col items-center justify-center min-h-screen bg-cover bg-top"
+        style={{ backgroundImage: `url(${welcomeImg})`, backgroundSize: "auto 100vh"}}>
             <div className="bg-gray-700 bg-opacity-50 p-6 rounded-lg items-center flex flex-col">
                 <h1 className="text-4xl font-bold text-white pb-4">VRScans Materials Library</h1>
                 <p className="text-lg text-white">An intuitive, React-powered application that dynamically fetches and displays VRScans materials via a pre-configured REST API.</p>
