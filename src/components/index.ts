ccacd6b5f8fbc6ab710c4b6ecb929b540f8898d0
Martin Andreev <martin_i_a@abv.bg>
2024-11-30 19:47:18 +0200

    Add the import for the Navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as ProductsSidebar } from "./ProductsSidebar";
 export { default as ProfileSidebar } from "./ProfileSidebar";
 export { default as CollectionsSidebar } from "./CollectionsSidebar";
 export { default as ErrorElement } from "./ErrorElement";
+export { default as Navbar } from "./navbar/Navbar";
