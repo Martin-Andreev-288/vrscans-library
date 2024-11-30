00b5fef101d99796c9f9eaf23fa6950f4d516468
Nia Kostadinova <n_deyanova@abv.bg>
2024-11-30 16:59:47 +0200

    Add logo to assets

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -7,7 +7,7 @@ const Welcome: React.FC = () => {
       className="flex flex-col items-center justify-center min-h-screen bg-cover bg-top"
       style={{ backgroundImage: `url(${welcomeImg})`, backgroundSize: "auto 100vh" }}
     >
-      <div className="bg-gray-600 bg-opacity-70 p-20 rounded-lg items-center flex flex-col">
+      <div className="bg-gray-600 bg-opacity-70 p-20 rounded-3xl items-center flex flex-col">
         <h1 className="text-5xl font-bold text-white pb-6">VRScans Materials Library</h1>
         <p className="text-lg text-white pb-40">
           An intuitive, React-powered application that dynamically fetches and displays VRScans
