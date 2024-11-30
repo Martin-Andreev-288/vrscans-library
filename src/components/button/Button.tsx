a92f7b6f4ce6506c9abb0b43098653f06873551c
Nia Kostadinova <n_deyanova@abv.bg>
2024-11-30 15:59:58 +0200

    Add buttons to Welcome page

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from "react";
 
 type ButtonProps = {
   children: ReactNode;
-  type: "navButton";
+  type: "navButton" | "welcomeButton";
 };
 
 export default function Button({ children, type }: ButtonProps) {
@@ -10,7 +10,9 @@ export default function Button({ children, type }: ButtonProps) {
     "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
-    navButton: base + " px-4 py-3 md:px-6 md:py-4"
+    navButton: base + " px-4 py-3 md:px-6 md:py-4",
+    welcomeButton:
+      "border border-black rounded-full bg-white inline-flex items-center justify-center py-3 px-5 text-center text-sm font-medium text-black hover:bg-body-color hover:border-body-color"
   };
 
   return <button className={styles[type]}>{children}</button>;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,4 +1,5 @@
 import welcomeImg from "../assets/welcome-page-img-2.png";
+import Button from "../components/button/Button";
 
 const Welcome: React.FC = () => {
   return (
@@ -6,12 +7,16 @@ const Welcome: React.FC = () => {
       className="flex flex-col items-center justify-center min-h-screen bg-cover bg-top"
       style={{ backgroundImage: `url(${welcomeImg})`, backgroundSize: "auto 100vh" }}
     >
-      <div className="bg-gray-700 bg-opacity-50 p-6 rounded-lg items-center flex flex-col">
-        <h1 className="text-4xl font-bold text-white pb-4">VRScans Materials Library</h1>
-        <p className="text-lg text-white">
+      <div className="bg-gray-600 bg-opacity-70 p-20 rounded-lg items-center flex flex-col">
+        <h1 className="text-5xl font-bold text-white pb-6">VRScans Materials Library</h1>
+        <p className="text-lg text-white pb-40">
           An intuitive, React-powered application that dynamically fetches and displays VRScans
           materials via a pre-configured REST API.
         </p>
+        <div className="items-center flex">
+          <Button type="welcomeButton">Login / Signup</Button>
+          <Button type="welcomeButton">Explore VRScans</Button>
+        </div>
       </div>
     </div>
   );
