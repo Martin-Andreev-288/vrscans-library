d9d718cdc86c466993396b274610fa4d4891562f
Nia Kostadinova <n_deyanova@abv.bg>
2024-11-30 12:50:55 +0200

    Add welcome page and assets

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,20 @@
+import welcomeImg from "../assets/welcome-page-img-2.png";
+
 const Welcome: React.FC = () => {
-  return <div>Welcome</div>;
+  return (
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-cover bg-top"
+      style={{ backgroundImage: `url(${welcomeImg})`, backgroundSize: "auto 100vh" }}
+    >
+      <div className="bg-gray-700 bg-opacity-50 p-6 rounded-lg items-center flex flex-col">
+        <h1 className="text-4xl font-bold text-white pb-4">VRScans Materials Library</h1>
+        <p className="text-lg text-white">
+          An intuitive, React-powered application that dynamically fetches and displays VRScans
+          materials via a pre-configured REST API.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
