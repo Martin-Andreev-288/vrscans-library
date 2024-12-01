5f70ebfa6060eda907370feb1339b52ac4bc1ca0
Nia Kostadinova <n_deyanova@abv.bg>
2024-12-01 21:59:43 +0200

    Add SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,49 @@
+import logo from "../assets/logo.png";
+import logInImg from "../assets/sign-in-img.png";
+import DefaultInput from "../components/defaultInput/DefaultInput";
+
 const SignUp: React.FC = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen flex flex-col justify-start items-center">
+      <div className="flex flex-col items-left justify-center border-b-4 border-b-black h-[8vh] w-[90vw]">
+        <img className="w-[25rem]" src={logo} alt="Chaosgroup Logo" />
+      </div>
+      <div className="flex flex-row items-center h-[90vh]">
+        <img className="h-[45vh] rounded-2xl mr-20" src={logInImg} alt="VR Scan Image" />
+        <div className="w-[25vw] h-[45vh] flex flex-col items-center">
+          <h2 className="text-4xl font-bold text-black pb-5">Sign Up</h2>
+          <form>
+            <div className="pb-3 w-[25vw]">
+              <DefaultInput placeholder="Username" />
+            </div>
+            <div className="pb-3 w-[25vw]">
+              <DefaultInput placeholder="Email" />
+            </div>
+            <div className="pb-3 w-[25vw]">
+              <DefaultInput placeholder="Password" />
+            </div>
+            <div className="pb-3 w-[25vw]">
+              <DefaultInput placeholder="Confirm Password" />
+            </div>
+            <div className="mb-5">
+              <input
+                type="submit"
+                value="Log In"
+                className="w-full cursor-pointer rounded-md border border-primary bg-black px-5 py-3 text-base font-medium text-white transition hover:bg-opacity-90"
+              />
+            </div>
+          </form>
+          <p className="mb-2 inline-block text-base text-dark hover:text-primary hover:underline dark:text-black">
+            Forgot Password?
+          </p>
+          <p className="text-base text-body-color dark:text-dark-6">
+            <span className="pr-0.5 text-gray-400">Not a member yet?</span>
+            <span className="text-primary hover:underline"> Sign Up</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
