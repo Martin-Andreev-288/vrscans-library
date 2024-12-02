df18597d5ad30611ff0da893a168dfa85f6e4f27
Nia Kostadinova <n_deyanova@abv.bg>
2024-12-02 14:21:10 +0200

    Add Profile & change css of Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -7,10 +7,10 @@ type ButtonProps = {
 
 export default function Button({ children, type }: ButtonProps) {
   const base =
-    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "inline-block text-xs rounded-full bg-black font-semibold tracking-wide text-white transition-colors duration-300 hover:bg-green focus:bg-green focus:outline-none disabled:cursor-not-allowed";
 
   const styles = {
-    navButton: base + " px-4 py-3 md:px-6 md:py-4",
+    navButton: base + " px-4 py-2",
     welcomeButton:
       "border border-black rounded-full bg-white inline-flex items-center justify-center py-3 px-5 text-center text-sm font-medium text-black hover:bg-body-color hover:border-body-color",
     logInButton:
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,25 @@
+import DefaultInput from "../components/defaultInput/DefaultInput";
+import Button from "../components/button/Button";
+
 const Profile: React.FC = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex flex-row items-center h-[90vh]">
+      <div className="w-[25vw] h-[45vh] flex flex-col items-center">
+        <h2 className="text-4xl font-bold text-black pb-10">Account Details</h2>
+        <form>
+          <div className="pb-3 w-[25vw]">
+            <DefaultInput placeholder="Email" />
+          </div>
+          <div className="pb-3 w-[25vw]">
+            <DefaultInput placeholder="Password" />
+          </div>
+          <div className="mb-10">
+            <Button type="navButton">Log Out</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
