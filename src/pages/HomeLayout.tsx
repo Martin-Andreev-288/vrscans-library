29f55d88e6d9564a0f9293f672e44e00575ca97b
Martin Andreev <martin_i_a@abv.bg>
2024-12-11 16:26:03 +0200

    Delete duplicated and redundant CSS code

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -5,13 +5,11 @@ export default function HomeLayout() {
   return (
     <div className="flex flex-col items-center pt-9">
       {/* Navbar */}
-      <nav className="w-full">
-        <div className="w-full max-w-xl md:max-w-2xl lg:max-w-4xl mx-auto">
-          <Navbar />
-        </div>
+      <nav className="w-full max-w-xl md:max-w-2xl lg:max-w-5xl mx-auto">
+        <Navbar />
       </nav>
       {/* Main Content */}
-      <main className="bg-white p-4 rounded-lg">
+      <main className="w-full max-w-xl md:max-w-2xl lg:max-w-5xl mx-auto">
         <Outlet />
       </main>
     </div>
