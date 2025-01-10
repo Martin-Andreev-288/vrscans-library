b3ce79e7187ab4c7587d91d4b51e17c7465e84a5
Martin Andreev <martin_i_a@abv.bg>
2025-01-11 00:18:11 +0200

    Adjust the logout button to work and do additional slight modifications in the profile page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { DefaultInput, Button } from "../../components";
 import { ProfileSidebar } from "../../features";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../store/slices/userSlice";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    dispatch(logoutUser());
+    toast.success("Logout successful!");
+    return new Promise((resolve) => setTimeout(() => resolve(navigate("/")), 2000));
+  };
+
   return (
     <div className="main">
       <aside>
@@ -11,16 +25,19 @@ export default function Profile() {
         <h2 className="text-4xl font-bold text-black pb-10">Account Details</h2>
         <form>
           <div className="pb-3 w-[25vw]">
-            <DefaultInput placeholder="Email" />
+            <DefaultInput type="text" name="email" placeholder="Email" />
           </div>
           <div className="pb-3 w-[25vw]">
-            <DefaultInput placeholder="Password" />
+            <DefaultInput type="password" name="password" placeholder="Password" />
           </div>
           <div className="mb-10">
-            <Button type="navButton">Log Out</Button>
+            <Button type="navButton" onClick={handleLogout}>
+              Log Out
+            </Button>
           </div>
         </form>
       </div>
+      <ToastContainer position="top-center" />
     </div>
   );
 }
