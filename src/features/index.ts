2a1b0fdf49589ef2a610d7fcda54b25ed4195efb
Martin Andreev <martin_i_a@abv.bg>
2025-01-13 22:52:19 +0200

    Replace the ProductCard component in the features folder (it was placed in wrong place) and update the corresponding imports

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,5 +1,5 @@
 export { default as CollectionsCard } from "./collections/CollectionsCard";
 export { default as CollectionsSidebar } from "./collections/CollectionsSidebar";
-export { default as ProductCard } from "../components/ProductCard/ProductCard";
+export { default as ProductCard } from "./products/ProductCard";
 export { default as ProductsFilters } from "./products/ProductsFilters";
 export { default as ProfileSidebar } from "./profile/ProfileSidebar";
