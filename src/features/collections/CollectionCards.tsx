b834ca17728b613855dd209b2aab663a148bdc7e
Martin Andreev <martin_i_a@abv.bg>
2025-01-17 14:31:44 +0200

    Implement the like and the dislike functionality in the collection lists

--- src/features/collections/CollectionCards.tsx
+++ src/features/collections/CollectionCards.tsx
@@ -3,6 +3,8 @@ import { Button } from "../../components";
 import { FiTrash } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { removeCollection, removeItemFromCollection } from "../../store/slices/collectionsSlice";
+import { addToFavs, removeFromFavs } from "../../store/slices/favoritesSlice";
+import { type VRScan } from "../../utils/types";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 
@@ -14,6 +16,12 @@ type CollectionCardsProps = {
 export default function CollectionCards({ viewingItems, setViewingItems }: CollectionCardsProps) {
   const collections = useSelector((state: RootState) => state.collections);
   const dispatch = useDispatch();
+
+  const favoritesList = useSelector((state: RootState) => state.favItems);
+
+  const isFavorite = (itemId: number): boolean => {
+    return favoritesList.some((favItem: VRScan) => favItem.id === itemId);
+  };
 
   const emptyPageText = "No collections found. Add your first collection to get started.";
 
@@ -95,8 +103,18 @@ export default function CollectionCards({ viewingItems, setViewingItems }: Colle
                 >
                   <FiTrash />
                 </button>
-                <button className="absolute top-2 right-2 bg-gray-200 text-gray-600 rounded-full p-2 hover:bg-gray-300">
-                  ♥
+                <button
+                  className="absolute top-2 right-2 bg-gray-200 text-gray-600 rounded-full p-2 hover:bg-gray-300"
+                  onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+                    event.stopPropagation();
+                    if (isFavorite(item.id)) {
+                      dispatch(removeFromFavs(item.id));
+                    } else {
+                      dispatch(addToFavs(item));
+                    }
+                  }}
+                >
+                  {isFavorite(item.id) ? "♥" : "♡"}
                 </button>
 
                 {/* Image */}
