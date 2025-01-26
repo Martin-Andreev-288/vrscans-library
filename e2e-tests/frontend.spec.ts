2fe6c6317b9eb0f389bdaba772c4cc4c4d9aff70
Martin Andreev <martin_i_a@abv.bg>
2025-01-26 16:23:12 +0200

    Add e2e tests based on playwright

---
+++ e2e-tests/frontend.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from "@playwright/test";
+
+const appUrl = "https://martin-andreev-288.github.io";
+
+test.describe("landing page", () => {
+  test("logging in works", async ({ page }) => {
+    await page.goto(appUrl);
+
+    await page.getByRole("button", { name: "Login / Signup" }).click();
+
+    await page.getByRole("textbox", { name: "Email" }).fill("proba@gmail.com");
+    await page.getByRole("textbox", { name: "Password" }).fill("123456");
+    await page.getByRole("button", { name: "Log In" }).click();
+
+    await expect(page).toHaveURL(`${appUrl}/products`);
+    await expect(page.getByRole("button", { name: "Tihael" })).toBeVisible();
+  });
+});
+
+test.describe("products page", () => {
+  test("shows vrscans", async ({ page }) => {
+    await page.goto(appUrl);
+
+    await page.getByRole("button", { name: "Explore VRScans" }).click();
+
+    const items = page.locator(".card-container li");
+    await expect(items.first()).toBeVisible({ timeout: 5_000 });
+    const count = await items.count();
+    expect(count).toBeGreaterThan(1);
+  });
+
+  test("can filter materials", async ({ page }) => {
+    await page.goto(appUrl);
+    await page.getByRole("button", { name: "Explore VRScans" }).click();
+    const items = page.locator(".card-container li");
+    await expect(items.first()).toBeVisible({ timeout: 5_000 });
+
+    await page.getByRole("checkbox", { name: "Car Paint" }).click();
+
+    const filteredProducts = page.locator(".card-container li");
+    await expect(filteredProducts.first()).toContainText("Car Paint");
+    const productsCount = await filteredProducts.count();
+    for (let i = 0; i < productsCount; i++) {
+      const productText = await filteredProducts.nth(1).textContent();
+      expect(productText?.trim()).toContain("Car Paint");
+    }
+  });
+});
