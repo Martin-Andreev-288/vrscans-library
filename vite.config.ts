ff9a8ef481a8ea9fb1b2704c20cc590ccdd3d36e
Martin Andreev <martin_i_a@abv.bg>
2025-01-27 18:18:56 +0200

    Install the vitest coverage depencency

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,18 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/setupTests.ts"],
-    include: ["src/**/*.{test,spec}.{ts,tsx}"]
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+    coverage: {
+      include: [
+        "src/components/modalWrapper/ModalWrapper.tsx",
+        "src/components/loader/Loader.tsx",
+        "src/components/filterSelection/FilterSection.tsx",
+        "src/components/defaultInput/DefaultInput.tsx",
+        "src/components/button/Button.tsx",
+        "src/components/accessDenied/AccessDenied.tsx",
+        "src/features/products/ProductsFilters.tsx",
+        "src/features/products/ProductList.tsx"
+      ]
+    }
   }
 });
