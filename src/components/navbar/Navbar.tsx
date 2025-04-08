12fc41a54364b51842ba009cf172db064a6e1c33
Martin Andreev <martin_i_a@abv.bg>
2025-04-08 14:30:11 +0300

    Improve layout and full-width styling
    Update the navbar to span the full width of the viewport.
    Constrain the inner content (logo and nav buttons) to match the main content width on each page for better alignment and consistency.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,39 +9,39 @@ export default function Navbar() {
 
   return (
     <nav
-      className="flex items-center pt-2 justify-between w-full max-w-xl md:max-w-2xl lg:max-w-6xl mx-auto border-b-4 border-b-gray-200 pb-4 sticky top-0 z-50
+      className="w-full border-b-4 pt-2 border-b-gray-200 pb-4 sticky top-0 z-50
     bg-white/80 backdrop-blur-lg"
     >
-      <div>
+      <div className="flex items-center justify-between max-w-xl md:max-w-2xl lg:max-w-6xl mx-auto">
         <Logo />
+        {user ? (
+          <ul className="flex items-center gap-7">
+            <li>
+              <NavLink to="/favorites">
+                <Button type="navButton">Favorites</Button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/collections">
+                <Button type="navButton">Collections</Button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile">
+                <Button type="navButton">{user.username}</Button>
+              </NavLink>
+            </li>
+          </ul>
+        ) : (
+          <ul className="flex items-center gap-16">
+            <li>
+              <NavLink to="/login">
+                <Button type="navButton">Login</Button>
+              </NavLink>
+            </li>
+          </ul>
+        )}
       </div>
-      {user ? (
-        <ul className="flex items-center gap-7">
-          <li>
-            <NavLink to="/favorites">
-              <Button type="navButton">Favorites</Button>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/collections">
-              <Button type="navButton">Collections</Button>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">
-              <Button type="navButton">{user.username}</Button>
-            </NavLink>
-          </li>
-        </ul>
-      ) : (
-        <ul className="flex items-center gap-16">
-          <li>
-            <NavLink to="/login">
-              <Button type="navButton">Login</Button>
-            </NavLink>
-          </li>
-        </ul>
-      )}
     </nav>
   );
 }
